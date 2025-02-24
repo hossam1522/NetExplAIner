ccc77be58e578a543191a759fb7c6d71d8e591bc
Hossam El Amraoui <hossam@correo.ugr.es>
2025-02-24 18:12:00 +0100

    feat: added docstrings

--- netexplainer/dataset.py
+++ netexplainer/dataset.py
@@ -3,10 +3,19 @@ import os
 
 class Dataset:
     def __init__(self, path: str):
+        """
+        Initialize the dataset object with the path in which the files are located
+
+        Args:
+            path (str): The path in which the files are located
+        """
         self.__path = path
         self.__files = self.__get_files_from_path()
     
     def __get_files_from_path(self) -> list:
+        """
+        Get the files that contain the network data from the path
+        """
         files_in_path = os.listdir(self.__path)
         net_files = []
         for file in files_in_path:
