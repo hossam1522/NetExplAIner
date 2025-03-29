7e01d2262aa3212749ffa9422d237ce64965d274
Hossam El Amraoui <hossam@correo.ugr.es>
2025-03-29 14:09:31 +0100

    refactor: added docstrings and types

--- netexplainer/llm.py
+++ netexplainer/llm.py
@@ -5,7 +5,19 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 class LLM:
-    def __init__(self, data_path):
+    def __init__(self, data_path: str):
+        """
+        Initialize the LLM object with the file provided
+        Args:
+            data_path (str): The path of the file to process
+        """
+        if not os.path.exists(data_path):
+            raise FileNotFoundError(f'The path {data_path} does not exist')
+        elif not os.path.isfile(data_path):
+            raise FileExistsError(f'The path {data_path} is not a file, please provide a file')
+        elif not data_path.endswith('.txt'):
+            raise TypeError(f'The file {data_path} is not a text file, please provide a txt file')
+
         load_dotenv()
         loader = TextLoader(data_path)
         self.file = loader.load()
@@ -14,7 +26,12 @@ class LLM_GEMINI(LLM):
     """
     Class for Google Gemini LLM
     """
-    def __init__(self, data_path):
+    def __init__(self, data_path: str):
+        """
+        Initialize the LLM object with the file provided
+        Args:
+            data_path (str): The path of the file to process
+        """
         super().__init__(data_path)
         os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
