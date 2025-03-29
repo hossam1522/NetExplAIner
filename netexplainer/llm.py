5ec72f602c5f658cae5096df96f4cedf24b8764d
Hossam El Amraoui <hossam@correo.ugr.es>
2025-03-29 14:07:54 +0100

    refactor: removed comments

--- netexplainer/llm.py
+++ netexplainer/llm.py
@@ -7,17 +7,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 class LLM:
     def __init__(self, data_path):
         load_dotenv()
-        #os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
-
-        #self.model = ChatGoogleGenerativeAI(
-        #    model="gemini-2.0-flash",
-        #    temperature=0,
-        #    max_tokens=None,
-        #    timeout=None,
-        #    max_retries=2,
-        #    # other params...
-        #)
-
         loader = TextLoader(data_path)
         self.file = loader.load()
 
