2319875be8cb82b90086fa950e4956d94ccb3ffe
Hossam El Amraoui <hossam@correo.ugr.es>
2025-04-15 15:19:51 +0200

    feat: added downloading and cleaning data in arguments

--- netexplainer/__main__.py
+++ netexplainer/__main__.py
@@ -1,6 +1,7 @@
 from netexplainer.dataset import Dataset
 from netexplainer.llm import *
 from netexplainer.evaluator import Evaluator
+from netexplainer.scraper import Scraper
 import argparse
 import time
 
@@ -44,3 +45,17 @@ def test():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--download-data", action="store_true", help="Download network data from Wireshark")
+    group.add_argument("--clean-data", type=int, metavar="<N>", help="Keep network files with a maximum of N packets")
+
+    args = parser.parse_args()
+
+    if args.download_data:
+        scraper = Scraper()
+        scraper.download_captures()
+    elif args.clean_data:
+        max_packets = args.clean_data
+        scraper = Scraper()
+        scraper.clean_raw_data(max_packets=max_packets)
