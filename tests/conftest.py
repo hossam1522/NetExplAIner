e5c29592351cf1ae571afdcb9bd6791d3fc59451
Hossam El Amraoui <hossam@correo.ugr.es>
2025-04-30 00:23:49 +0200

    fix: solved unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,32 +1,26 @@
 import pytest
+from pathlib import Path
+import tempfile
 import logging
 
 @pytest.fixture(autouse=True)
-def configure_loggers(tmp_path):
-    log_file = tmp_path / "tests.log"
-
-    root_logger = logging.getLogger()
-    root_logger.handlers.clear()
-
-    formatter = logging.Formatter("%(levelname)s - %(message)s")
-
-    file_handler = logging.FileHandler(log_file)
-    file_handler.setFormatter(formatter)
-
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-
-    root_logger.addHandler(file_handler)
-    root_logger.addHandler(console_handler)
-    root_logger.setLevel(logging.DEBUG)
-
-    loggers = ["netexplainer", "scraper", "dataset", "llm"]
-    for name in loggers:
-        logger = logging.getLogger(name)
+def configure_logger():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        log_path = Path(tmpdir) / "tests.log"
+        logger = logging.getLogger("netexplainer")
         logger.handlers.clear()
+
+        formatter = logging.Formatter("%(levelname)s - %(message)s")
+
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(formatter)
+
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+
         logger.addHandler(file_handler)
         logger.addHandler(console_handler)
         logger.setLevel(logging.DEBUG)
         logger.propagate = False
 
-    yield
+        yield
